3800a79ae65573de196fb07154bdf2f28fc7c0b6
kurtwilliam <k.w.schneid@gmail.com>
2020-07-25 14:49:21 -0400

    isntalling youtube player

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,14 +8,43 @@ import {
   SafeAreaView,
   Platform,
   Button,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions
 } from "react-native";
 
 import { AntDesign } from "@expo/vector-icons";
-import { Video } from "expo-av";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+const API_KEY = "AIzaSyCdv2KFnYJWbo0LvwQ2BxXb-D0EHJM3MRM";
 
 const Main = () => {
   const [searchText, setSearchText] = useState("Search");
+  const [video, setVideo] = useState({});
+  const [playing, setPlaying] = useState(false);
+  const [screenDimensions, setScreenDimensions] = useState([0, 0]);
+  const playerRef = useRef(null);
+
+  const getVideo = () => {
+    return fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=${searchText}&key=[${API_KEY}])`
+    )
+      .then(response => response.json())
+      .then(jsonObj => setVideo(jsonObj))
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    setScreenDimensions([
+      Dimensions.get("window").width,
+      Dimensions.get("window").height
+    ]);
+
+    console.log("fetch", getVideo());
+    console.log(video);
+  }, []);
+  console.log("video", video);
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -26,15 +55,21 @@ const Main = () => {
           onChangeText={text => setSearchText(text)}
         />
         <View style={styles.videoContainer}>
-          <Video
-            style={styles.video}
-            source={{
-              uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+          <YoutubePlayer
+            ref={playerRef}
+            height={(screenDimensions[0] * 9) / 16 - 16}
+            width={screenDimensions[0] - 16}
+            videoId={"AVAc1gYLZK0"}
+            play={playing}
+            onChangeState={event => console.log(event)}
+            onReady={() => console.log("ready")}
+            onError={e => console.log(e)}
+            // volume={100}
+            playbackRate={1}
+            initialPlayerParams={{
+              cc_lang_pref: "us",
+              showClosedCaptions: false
             }}
-            rate={1.0}
-            volume={1.0}
-            isMuted={false}
-            resizeMode="contain"
           />
           <TouchableHighlight
             onPress={() => {
